9d0463fd67b48a3c4e35ee8c6ab6cb0a95c17049
adpangelinan <36466227+adpangelinan@users.noreply.github.com>
2020-01-26 10:47:33 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
 // JavaScript source code
 
 
-document.getElementById("unlogged").addEventListener("mouseover", changeColor)
+document.getElementById("userSearch").value="What are you looking for?"
 
 
-function changeColor() {
-    console.log("Something is happening")
-    document.getElementById("unlogged").style.color = "red"
-   
+function showTOS() {
+
+    
+    var x = document.getElementById("TOS");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+} 
+
+function userSearch() {
+
+    var search = document.getElementById("userSearch").value
+
+    window.open('https://google.com/search?q='+search)
+
+    console.log(search)
+
+
+
+}
+
+
+function getDate() {
+
+    var myDate = new Date()
+
+    
+
+    return myDate.getFullYear()
 
 }
 
 
 
+console.log("The year is " + getDate())
+
+var x = document.getElementById("Year")
+
+alert(x.innerHTML)
