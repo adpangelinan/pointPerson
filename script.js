10b1cd32da101f539593eb837e570b72d06ce8bf
adpangelinan <adrian.pang92@outlook.com>
2020-01-26 10:45:31 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+
+
+document.getElementById("unlogged").addEventListener("mouseover", changeColor)
+
+
+function changeColor() {
+    console.log("Something is happening")
+    document.getElementById("unlogged").style.color = "red"
+   
+
+}
+
+
+
